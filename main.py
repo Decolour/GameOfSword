6469a8fe1bcc02c4b3e316b8ac9e83d71de1ced1
Decolour <decolourrr@gmail.com>
2024-07-17 03:25:18 +0300

    upd

--- main.py
+++ main.py
@@ -54,20 +54,58 @@ class Dagger(Weapon):
         print("Dagger attack")
 
 # Создание объектов оружия
-Kladinec = Sword("Кладинец", 10, 10) # Create a sword object
-Fang = Dagger("Клык", 5) # Create a dagger object
-Kirdyk = Axe("Кирдык", 7) # Create an axe object
+Kladinec = Sword("Кладинец", 10, 10)
+Fang = Dagger("Клык", 5)
+Kirdyk = Axe("Кирдык", 7)
 
 # Создание объектов бойцов и монстров
-Vanya = Fighter("Ваня", 4, Kladinec, 10, 11, 100) # Create a fighter object
-Ilyusha = Fighter("Илюша", 7, Kirdyk, 15, 7, 70) # Create a fighter object
-Petrovich = Fighter("Петрович", 15, Fang, 7, 10, 50) # Create a fighter object
+Vanya = Fighter("Ваня", 4, Kladinec, 10, 11, 100)
+Ilyusha = Fighter("Илюша", 7, Kirdyk, 15, 7, 70)
+Petrovich = Fighter("Петрович", 20, Fang, 7, 10, 50)
 
-Babay = Monster("Бабай", 10, Kladinec, 10, 10, 40) # Create a monster object
-Byaka = Monster("Бяка", 5, Fang, 6, 5, 60) # Create a monster object
+Babay = Monster("Бабай", 10, Kladinec, 10, 10, 100)
+Byaka = Monster("Бяка", 5, Fang, 13, 53, 60)
+
+def change_warrior():
+    print("Имеющиеся бойцы:")
+    print("1. Ваня")
+    print("2. Илюша")
+    print("3. Петрович")
+    choice = input("Выберите бойца (Введите его номер): ")
+    if choice == "1":
+        warrior = Vanya
+        print("Вы выбрали Ваню")
+    elif choice == "2":
+        warrior = Ilyusha
+        print("Вы выбрали Илюшу")
+    elif choice == "3":
+        warrior = Petrovich
+        print("Вы выбрали Петровича")
+    else:
+        print("Неверный выбор")
+        return None
+    return warrior
+
+def change_weapon(warrior):
+    print("Имеющееся оружие:")
+    print("1. Кладинец")
+    print("2. Клык")
+    print("3. Кирдык")
+    choice = input("Выберите новое оружие (Введите его номер): ")
+    if choice == "1":
+        warrior.weapon = Kladinec
+        print("Вы выбрали Кладинец")
+    elif choice == "2":
+        warrior.weapon = Fang
+        print("Вы выбрали Клык")
+    elif choice == "3":
+        warrior.weapon = Kirdyk
+        print("Вы выбрали Кирдык")
+    else:
+        print("Неверный выбор")
 
 def damage_calculation(attacking, defending):
-    damage = attacking.weapon.damage + attacking.Strength - defending.Defense + random.randint(0, 10) * attacking.luck
+    damage = attacking.weapon.damage + attacking.Strength - defending.Defense + random.randint(0, 3) * attacking.luck
     return max(damage, 0)  # Чтобы не было отрицательного урона
 
 def fight(attacking, defending):
@@ -88,11 +126,14 @@ def fight(attacking, defending):
         if attacking.Health <= 0:
             print(f"{defending.name} победил {attacking.name}")
 
-        # Сброс параметров объектов до начальных значений
+    # Сброс параметров объектов до начальных значений
     attacking.Health = initial_attacking_health
     defending.Health = initial_defending_health
 
+# Выбор бойца и смена оружия
+selected_warrior = change_warrior()
+if selected_warrior:
+    change_weapon(selected_warrior)
 
-fight(Vanya, Babay)
-fight(Ilyusha, Byaka)
-fight(Petrovich, Babay)
+# Запуск боя
+fight(selected_warrior, Babay)
